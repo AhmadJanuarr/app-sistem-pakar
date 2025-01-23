a2d7e9ae745412bf5f6418dbb099ba03636e3576
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-24 02:29:24 +0700

    feat: add user_data.php for retrieving user and related statistics in admin panel

---
+++ includes/admin/user/user_data.php
@@ -0,0 +1,14 @@
+<?php
+$queryPasien = mysqli_query($koneksi, "SELECT * FROM user WHERE role = '1'");
+
+$jumlahPasien = mysqli_query($koneksi, "SELECT COUNT('id_user') as jml_pasien FROM user WHERE role='1'");
+$pasien = mysqli_fetch_assoc($jumlahPasien);
+
+$jumlahPenyakit = mysqli_query($koneksi, "SELECT COUNT('id_penyakit') as jml_penyakit FROM penyakit");
+$penyakit = mysqli_fetch_assoc($jumlahPenyakit);
+
+$jumlahGejala = mysqli_query($koneksi, "SELECT COUNT('id_gejala') as jml_gejala FROM gejala");
+$gejala = mysqli_fetch_assoc($jumlahGejala);
+
+$jumlahSolusi = mysqli_query($koneksi, "SELECT COUNT('id_solusi') as jml_solusi FROM solusi");
+$solusi = mysqli_fetch_assoc($jumlahSolusi);
