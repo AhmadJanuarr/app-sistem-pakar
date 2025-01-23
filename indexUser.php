aa8e69830902605651833990795d86e09d19edbe
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-24 02:21:37 +0700

    refactor: modularize indexUser.php by including header, sidebar, user data, and footer for improved maintainability

---
+++ indexUser.php
@@ -0,0 +1,7 @@
+<?php
+include "function.php";
+include "includes/admin/header.php";
+include "includes/admin/sidebar.php";
+include "includes/admin/user/user_data.php";
+include "includes/admin/user/user_view.php";
+include "includes/admin/footer.php";
