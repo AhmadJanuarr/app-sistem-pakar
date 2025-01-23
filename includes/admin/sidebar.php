9c1c38ed36a916e6fbdcf2fea8e6bd8c16b2b7ba
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-24 02:26:46 +0700

    feat: add sidebar.php for admin dashboard with navigation links for user data and solutions

---
+++ includes/admin/sidebar.php
@@ -0,0 +1,42 @@
+<div class="kiri">
+    <section class="logo">
+        <img src="gambar/logo.png" alt="logo" height="70px" />
+    </section>
+    <div class="sidebar-heading">
+        <h5 class="font-weight-bold text-white text-uppercase teks">Data User</h5>
+    </div>
+    <section class="isi">
+        <a class="nav-link" href="indexUser.php">
+            <span>Data Pasien</span></a>
+    </section>
+    <section class="isi">
+        <a class="nav-link" href="indexPakar.php">
+            <span>Data Pakar</span></a>
+    </section>
+    <div class="sidebar-heading">
+        <h5 class="font-weight-bold text-white text-uppercase teks">Gejala & Penyakit</h5>
+    </div>
+    <section class="isi">
+        <a class="nav-link" href="indexPenyakit.php">
+            <span>Data Penyakit</span>
+        </a>
+    </section>
+    <section class="isi">
+        <a class="nav-link" href="indexGejala.php">
+            <span>Data Gejala</span>
+        </a>
+    </section>
+    <div class="sidebar-heading">
+        <h5 class="font-weight-bold text-white text-uppercase teks">Solusi</h5>
+    </div>
+    <section class="isi">
+        <a class="nav-link" href="indexSolusi.php">
+            <span>Data Solusi</span>
+        </a>
+    </section>
+    <section class="isi">
+        <a class="nav-link" href="logout.php">
+            <span>Logout</span>
+        </a>
+    </section>
+</div>
