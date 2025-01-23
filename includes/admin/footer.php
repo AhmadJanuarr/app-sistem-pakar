f7788afc45ddd50e2c8c9ad837122cc9d061271d
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-01-24 02:26:30 +0700

    feat: add footer.php with jQuery and Bootstrap scripts for improved functionality

---
+++ includes/admin/footer.php
@@ -0,0 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
